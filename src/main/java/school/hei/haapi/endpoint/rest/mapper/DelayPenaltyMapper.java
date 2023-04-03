e8f4b44dd1c452bb0b6c54bb0691bb3ef1138874
Sarobidy-23 <hei.sarobidy@gmail.com>
2023-04-03 10:58:28 +0300

    feat: testv2

--- src/main/java/school/hei/haapi/endpoint/rest/mapper/DelayPenaltyMapper.java
+++ src/main/java/school/hei/haapi/endpoint/rest/mapper/DelayPenaltyMapper.java
@@ -5,11 +5,14 @@ import org.springframework.stereotype.Component;
 import school.hei.haapi.endpoint.rest.model.CreateDelayPenaltyChange;
 import school.hei.haapi.model.DelayPenalty;
 import school.hei.haapi.endpoint.rest.model.DelayPenalty.InterestTimerateEnum;
+import school.hei.haapi.model.User;
 import school.hei.haapi.model.validator.CreateUpdateDelayPenaltyValidator;
+import school.hei.haapi.repository.UserRepository;
 
 @Component
 @AllArgsConstructor
 public class DelayPenaltyMapper {
+    private final UserRepository userRepository;
     private final CreateUpdateDelayPenaltyValidator delayPenaltyValidator;
     public school.hei.haapi.endpoint.rest.model.DelayPenalty toRest(DelayPenalty domain) {
         return new school.hei.haapi.endpoint.rest.model.DelayPenalty()
@@ -22,12 +25,14 @@ public class DelayPenaltyMapper {
     }
 
     public DelayPenalty toDomainDelayPenalty(CreateDelayPenaltyChange rest) {
+        User specifiedUser = userRepository.getById(rest.getStudentId());
         delayPenaltyValidator.accept(rest);
         return DelayPenalty.builder()
                 .interestPercent(rest.getInterestPercent())
                 .interestTimerate(InterestTimerateEnum.valueOf(rest.getInterestTimerate().toString()))
                 .graceDelay(rest.getGraceDelay())
                 .applicabilityDelayAfterGrace(rest.getApplicabilityDelayAfterGrace())
+            .student(specifiedUser)
                 .build();
     }
 
--- src/main/resources/db/testdata/V99_5__Test_create_penality_change.sql
+++ src/main/resources/db/testdata/V99_5__Test_create_penality_change.sql
@@ -3,4 +3,3 @@ insert into "create_delay_penalty_change"
 values
     ('penality1_id',null, 2, 'DAILY', 5, 5, '2023-01-15T08:25:25.00Z', 'GLOBAL'),
     ('penality2_id', 'student1_id', 2, 'DAILY', 7, 5, '2023-01-15T08:25:25.00Z', 'SPECIFIC');
-    2023-04-03 06:23:20.502091
